da9a787a9b260818a444178ecda8256c534f3c2d
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2026-01-04 22:22:31 -0500

    refactor: update hash_file to use compio and direct File handle
    - Switch from  to  for async I/O.
    - Change return type to .
    - Utilize  for efficient buffer reading.
    - Integrated  for capacity allocation.

--- src/hash.rs
+++ src/hash.rs
@@ -1,19 +1,19 @@
-use crate::models::HashString;
-use std::hash::Hash;
-use std::path::Path;
+use crate::files;
+use crate::models::HexStirng;
+use compio::{fs::File, io::AsyncReadAtExt};
 
-pub async fn hash_file(
-    file_path: &Path,
-) -> Result<(HashString, String), Box<dyn std::error::Error>> {
-    // create a hash that represent the file
+pub async fn hash_file(file: &File) -> Result<HexStirng, Box<dyn std::error::Error>> {
+    let file_meta = files::get_meta(&file).await?;
+    let (_, buffer) = file
+        .read_to_end_at(Vec::with_capacity(file_meta.len() as usize), 0)
+        .await
+        .unwrap();
 
-    let hash_string = HashString(file_path.to_string_lossy().to_string());
     let hash_value = {
-        use std::collections::hash_map::DefaultHasher;
-        use std::hash::Hasher;
-        let mut hasher = DefaultHasher::new();
-        hash_string.hash(&mut hasher);
-        format!("{:x}", hasher.finish())
+        use sha2::{Digest, Sha256};
+        let mut sha256_hasher = Sha256::new();
+        sha256_hasher.update(&buffer);
+        format!("{:x}", sha256_hasher.finalize())
     };
-    Ok((hash_string, hash_value))
+    Ok(HexStirng(hash_value))
 }
