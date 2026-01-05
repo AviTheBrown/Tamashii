861118768df3e3e7a523f1e11bd3d6c2fe097b5f
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2026-01-05 15:53:11 -0500

    feat: reorganize project structure and integrate FileRecord
    - Add  and  modules.
    - Import  model for use in main.
    - Remove  call from argument collection.
    - Clean up whitespace in main execution flow.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
+mod errors;
 mod files;
 mod hash;
+mod macros;
 mod models;
+use models::FileRecord;
 use std::path::Path;
 
 #[compio::main]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
-    dbg!(&args);
     let file_path = Path::new(&args[1]);
+
     let file = files::get_file(file_path)
         .await
         .expect("Failed to open file");
+    let meta = files::get_meta(&file)
+        .await
+        .expect("Failed to retreive file metadata.");
     let hashed_content = hash::hash_file(&file).await.unwrap();
 
     // args.remove(1)
     let file_hash_stuff = models::HashedFileInfo::new(args[1].clone(), hashed_content);
-    println!(
-        "INFO: \nFile Name{:?}\nHashed Content: {:?}",
-        file_hash_stuff.file_name, file_hash_stuff.hashed_content.0
-    );
+    let record = FileRecord {
+        path: file_path.to_path_buf(),
+        hash: file_hash_stuff.hashed_content,
+        size: (meta.len() as u8),
+        time_stamp: meta.created().expect("Failed to get creation time"),
+    };
+    println!("{}", record);
 }
