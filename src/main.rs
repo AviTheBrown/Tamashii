e708c1e530d80d3bfe9d1a3d7ca0b8e7e55d4f68
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2026-01-04 22:24:28 -0500

    feat: add CLI argument support for file hashing
    - Collect command-line arguments to specify the target file.
    - Use  to open the provided path.
    - Pass the file handle to the hashing function.
    - Register the new  module.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+mod files;
 mod hash;
 mod models;
+use std::path::Path;
 
 #[compio::main]
 async fn main() {
-    let (hash, value) = hash::hash_file().await.unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    dbg!(&args);
+    let file_path = Path::new(&args[1]);
+    let file = files::get_file(file_path)
+        .await
+        .expect("Failed to open file");
+    let hashed_content = hash::hash_file(&file).await.unwrap();
+
+    // args.remove(1)
+    let file_hash_stuff = models::HashedFileInfo::new(args[1].clone(), hashed_content);
+    println!(
+        "INFO: \nFile Name{:?}\nHashed Content: {:?}",
+        file_hash_stuff.file_name, file_hash_stuff.hashed_content.0
+    );
 }
