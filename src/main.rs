1f95786eaf7f1ed345a33d78bccb194c4cd7e675
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2026-01-18 16:50:26 -0500

    added unit test

--- src/main.rs
+++ src/main.rs
@@ -20,30 +20,19 @@ async fn main() -> Result<(), Exn<InitError>> {
     let file_path = Path::new(&args[1]);
 
     // get file
-    let file = files::get_file(file_path).await.map_err(|err| InitError {
+    let file = files::get_file(&file_path).await.map_err(|err| InitError {
         message: format!("There was an error trying to use the file: {}", err),
     })?;
     // retrieve metadata of file
-    let meta = files::get_meta(&file, &file_path)
-        .await
-        .map_err(|err| InitError {
-            message: format!("Failed to retriece metadata: {}", err),
-        })?;
+    let meta = files::get_meta(&file).await.map_err(|err| InitError {
+        message: format!("Failed to retriece metadata: {}", err),
+    })?;
     // hash the contents of the file
-    let hashed_file_content = hash::hash_file(&file, &file_path).await.map_err(|err| {
+    let hashed_file_content = hash::hash_file(&file).await.map_err(|err| {
         Exn::new(InitError {
             message: format!("Failed to hash {:?}'s contents: {}", file_path, err),
         })
     })?;
-
-    // create a instace of FileRecord
-    // let record = FileRecord::new(
-    //     file_path.to_path_buf(),
-    //     hashed_file_content,
-    //     // file_hash_stuff.hashed_content,
-    //     meta.len() as u8,
-    //     meta.created().expect("Failed to get creation time"),
-    // );
 
     let mut test_db = Database::get_or_create_db(DB_PATH).await?;
     test_db
@@ -67,4 +56,68 @@ async fn main() -> Result<(), Exn<InitError>> {
         })
     })?;
     Ok(())
+}
+#[cfg(test)]
+mod test {
+    use chrono::Utc;
+    use exn::ResultExt;
+
+    use super::*;
+    use crate::{
+        hash::hash_bytes,
+        models::{HexStirng, VERSION},
+    };
+    use std::{
+        env::{current_dir, temp_dir},
+        io::Write,
+        path::PathBuf,
+    };
+
+    #[compio::test]
+    async fn create_db() -> Result<(), Exn<InitError>> {
+        let db = Database::get_or_create_db(DB_PATH).await?;
+        println!("The db: {:?}", db);
+        Ok(())
+    }
+    #[compio::test]
+    async fn create_file() -> Result<(), Exn<errors::IoError<PathBuf>>> {
+        let tmp = tempfile::tempdir().map_err(|err| errors::IoError {
+            path: None,
+            message: format!("{}", err),
+        })?;
+        let path = tmp.path().join("test.rs");
+        let _ = files::get_file(&path).await;
+        Ok(())
+    }
+    #[compio::test]
+    async fn hash_known_input() {
+        let input = b"random-input-input";
+        let h1 = hash_bytes(input);
+        let h2 = hash_bytes(input);
+        assert_eq!(h1, h2)
+    }
+    #[compio::test]
+    async fn build_db() -> Result<(), Exn<InitError>> {
+        let test_db = Database::new().or_raise(|| InitError {
+            message: "Failed trying to create a new DB instance".into(),
+        })?;
+        let current_dir = std::env::current_dir().map_err(|err| {
+            Exn::new(InitError {
+                message: format!("Failed to get current directory: {}", err),
+            })
+        })?;
+        assert_eq!(test_db.version, VERSION);
+        assert_eq!(test_db.root_dir, PathBuf::from(current_dir));
+        Ok(())
+    }
+    #[compio::test]
+    async fn save_db() -> Result<(), Exn<InitError>> {
+        let test_db = Database::new().or_raise(|| InitError {
+            message: "Failed trying to create a new DB instance".into(),
+        })?;
+        let _ = test_db.save().await.or_raise(|| InitError {
+            message: "Failed to save DB".into(),
+        });
+        Ok(())
+    }
 }
